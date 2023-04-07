c62e02598266ac956a08d044c07b13de19b2b813
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2023-04-07 12:52:05 -0600

    Update 4-7-23 functions check.py

--- 4-7-23 functions check.py
+++ 4-7-23 functions check.py
@@ -15,8 +15,14 @@ def monsetrgen():
 quit = False
 
 while quit == False:  
-    e = input("Type quit to end loop")
+    e = input("Type quit to end loop ")
     if e == "quit":
         quit = True
     else:
         monsetrgen()
+
+def ATDS(x,y):
+    r = ((x+y)+2)/6
+    return r
+
+print(ATDS(5.0,2.5))
